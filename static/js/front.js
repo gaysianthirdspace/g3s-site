1f782c50696790d382ea66258e376cba8040ecfe
Ethan Li <lietk12@gmail.com>
2017-11-22 07:52:07 -0800

    Increased carousel dragging responsiveness.

--- static/js/front.js
+++ static/js/front.js
@@ -81,7 +81,7 @@ function sliders () {
     $('.homepage').owlCarousel({
       navigation: false, // Show next and prev buttons
       navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-      slideSpeed: 2000,
+      slideSpeed: 400,
       paginationSpeed: 1000,
       autoPlay: true,
       autoPlayHoverPause: true,
