6ce1edb8b70d7b27ed3a9b228b3f0fddd9cf6d5e
Ethan Li <lietk12@gmail.com>
2017-11-25 05:22:06 -0800

    Removed unnecessary scripts.

--- static/js/front.js
+++ static/js/front.js
@@ -11,7 +11,6 @@ $(function () {
   // productDetailSizes()
   utils()
   animations()
-  counters()
   contactForm()
 })
 
@@ -167,14 +166,6 @@ function animationsSlider () {
 
       console.log($(this).data('animate-always'))
     })
-  })
-}
-
-/* counters */
-function counters () {
-  $('.counter').counterUp({
-    delay: 10,
-    time: 1000
   })
 }
 
